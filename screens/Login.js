92c9b5a26823f914767bbd998706894ca54708a9
arinaggarwal1 <arinaggarwal1@gmail.com>
2022-01-04 00:58:43 -0500

    Initial Commit

---
+++ screens/Login.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import{
+    StyledContainer, 
+    InnerContainer,
+    PageLogo,
+    PageTitle
+} from './../components/styles'
+
+const Login = () => {
+    return (
+        <StyledContainer>
+            <StatusBar style = "dark"/>
+            <InnerContainer>
+                <PageLogo resizeMode = "cover" source = {require('./../assets/img/newLogo.png')} />
+                <PageTitle>Flower Crib</PageTitle>
+            </InnerContainer>
+        </StyledContainer>
+    );
+}
+
+export default Login; 
